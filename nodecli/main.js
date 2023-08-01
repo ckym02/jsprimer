a1304a2eb55ca1f08e17d9bcae4b85529c516665
koyama-c <koyama@linkedge.jp>
2023-08-02 00:21:39 +0900

    コマンドライン引数を処理する

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+// commanderモジュールからprogramオブジェクトをインポートする
+import { program } from "commander";
+
+// コマンドライン引数をcommanderでパースする
+program.parse(process.argv);
+
+// ファイルパスをprogram.args配列から取り出す
+const filePath = program.args[0];
+console.log(filePath);
+
+// jsprimer/nodecliディレクトリで
+// $ node main.js ./sample.md
+// ./sample.md
