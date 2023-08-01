77399c45702f6f4e13d97a5830bc9784000ccaed
koyama-c <koyama@linkedge.jp>
2023-08-02 00:35:28 +0900

    ファイルを読み込む

---
+++ nodecli/sampleMain.js
@@ -0,0 +1,27 @@
+import { program } from "commander";
+// Node.js標準モジュールのインポート
+// fs/promisesモジュールをfsオブジェクトとしてインポートする
+import * as fs from "node:fs/promises";
+
+// コマンドライン引数からファイルパスを取得する
+program.parse(process.argv);
+const filePath = program.args[0];
+
+// // ファイルを非同期で読み込む
+// fs.readFile(filePath).then(file => {
+//     // Bufferインスタンス
+//     console.log(file);
+// });
+
+// ファイルをUTF-8として非同期で読み込む
+fs.readFile(filePath, { encoding: "utf8" }).then(file => {
+  console.log(file);
+}).catch(err => {
+  console.error(err.message);
+  // 終了ステータス1（一般的なエラー）としてプロセスを終了させる
+  process.exit(1);
+});
+
+// /jsprimer/nodecli
+// $ node sampleMain.js notfound.md
+// ENOENT: no such file or directory, open 'notfound.md'
