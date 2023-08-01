cec75ce7ff63b9ed62ff9c6d69b59690ee1f5061
koyama-c <koyama@linkedge.jp>
2023-08-02 00:46:42 +0900

    gfmオプションを指定できるようにする

--- nodecli/sampleMain.js
+++ nodecli/sampleMain.js
@@ -5,9 +5,19 @@ import * as fs from "node:fs/promises";
 // markedモジュールからmarkedオブジェクトをインポートする
 import { marked } from "marked";
 
+// gfmオプションを定義する
+program.option("--gfm", "GFMを有効にする");
 // コマンドライン引数からファイルパスを取得する
 program.parse(process.argv);
 const filePath = program.args[0];
+
+// コマンドライン引数のオプションを取得する
+const options = program.opts();
+
+// コマンドライン引数で指定されなかったオプションにデフォルト値を上書きする
+const cliOptions = {
+    gfm: options.gfm ?? false,
+};
 
 // // ファイルを非同期で読み込む
 // fs.readFile(filePath).then(file => {
@@ -17,9 +27,11 @@ const filePath = program.args[0];
 
 // ファイルをUTF-8として非同期で読み込む
 fs.readFile(filePath, { encoding: "utf8" }).then(file => {
-  console.log(file);
   // MarkdownファイルをHTML文字列に変換する
-  const html = marked.parse(file);
+  const html = marked.parse(file, {
+    // オプションの値を使用する
+    gfm: cliOptions.gfm,
+  });
   console.log(html);
 }).catch(err => {
   console.error(err.message);
