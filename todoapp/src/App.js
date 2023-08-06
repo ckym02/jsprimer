edd4ef125afc117fda7b6b95d6e4211c7fb3d6b8
koyama-c <koyama@linkedge.jp>
2023-08-06 23:56:54 +0900

    削除機能の実装

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -22,8 +22,14 @@ export class App {
       todoItems.forEach(item => {
         // 完了済みならchecked属性をつけ、未完了ならchecked属性を外す
         const todoItemElement = item.completed
-          ? element`<li><input type="checkbox" class="checkbox" checked><s>${item.title}</s></li>`
-          : element`<li><input type="checkbox" class="checkbox">${item.title}</li>`;
+          ? element`<li><input type="checkbox" class="checkbox" checked>
+            <s>${item.title}</s>
+            <button class="delete">x</button>
+          </li>`
+          : element`<li><input type="checkbox" class="checkbox">
+            ${item.title}
+            <button class="delete">x</button>
+          </li>`;
         // チェックボックスがトグルしたときのイベントにリスナー関数を登録
         const inputCheckboxElement = todoItemElement.querySelector(".checkbox");
         inputCheckboxElement.addEventListener("change", () => {
@@ -31,6 +37,13 @@ export class App {
           this.#todoListModel.updateTodo({
             id: item.id,
             completed: !item.completed
+          });
+        });
+        // 削除ボタン(x)がクリックされたときにTodoListModelからアイテムを削除する
+        const deleteButtonElement = todoItemElement.querySelector(".delete");
+        deleteButtonElement.addEventListener("click", () => {
+          this.#todoListModel.deleteTodo({
+            id: item.id
           });
         });
         todoListElement.appendChild(todoItemElement);
